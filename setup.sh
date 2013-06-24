6618a65618d1468d5c284393cb6eca2a0f38734b
nrvana <eternalnrvana@gmail.com>
2013-06-23 22:30:23 -0400

    first commit

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cp -v reporting/base-email-item.html ../data/html/
 cp -v reporting/inbox.html ../data/html/inbox.html
 cp -v reporting/network-brief.html ../data/html/sections/
 
-# sed/echo cmd to edit conf/reporting.conf
+# echo cmd to edit conf/reporting.conf
 echo "" >> ../conf/reporting.conf
 echo "[reportinbox]" >> ../conf/reporting.conf
 echo "enabled = on" >> ../conf/reporting.conf
